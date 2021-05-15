ce91435be72b8a7e08b1662fe3678d9d923f8f49
Rahul-krishnan02 <82837281+Rahul-krishnan02@users.noreply.github.com>
2021-05-15 23:38:05 +0530

    Create question15.py

---
+++ question15.py
@@ -0,0 +1,13 @@
+class hello_world:
+    def print_hw(self):
+        print("hello world")
+
+
+
+class intro(hello_world):
+    def print_intro(self):
+        print("HI IA M A STUDENT OF MIT")
+
+rahul = intro()
+rahul.print_intro()
+rahul.print_hw()
